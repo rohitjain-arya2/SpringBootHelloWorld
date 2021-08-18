6210f6c2db7497192d8da3dc97f61c3961146d7b
Rohit Jain <rohitjain.arya2@gmail.com>
2021-08-18 11:57:45 +0530

    Update SpringBootHelloWorldApplication.java

--- src/main/java/com/javainuse/SpringBootHelloWorldApplication.java
+++ src/main/java/com/javainuse/SpringBootHelloWorldApplication.java
@@ -2,9 +2,17 @@ package com.javainuse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootHelloWorldApplication {
+
+	@GetMapping
+	 public String message(){
+       return "Azure Demo";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
